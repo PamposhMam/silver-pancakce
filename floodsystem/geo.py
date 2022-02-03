606705655a3c234c9de36cf1cbd08c8aae5bb702
Chloe <nfcc2@cam.ac.uk>
2022-02-03 12:42:07 +0000

    Complete Task 1D (Part 2)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def stations_by_river(stations):
+    """This function returns a dictionary that maps river names to a list of stations on that river"""
+
+    rivers = {}
+
+    for station in stations:
+        river = station.river
+
+        if river not in rivers:
+            rivers[river] = []
+        
+        rivers[river].append(station)
+    return rivers
+
+
+def rivers_by_station_number(stations, N):
+    """This function returns the N rivers with the greatest number of monitoring stations"""
+    return 
