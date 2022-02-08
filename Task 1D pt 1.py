4a78c1e69893ba0e875c1c4c028779942721a250
PamposhMam <99150494+PamposhMam@users.noreply.github.com>
2022-02-08 00:07:08 +0000

    Create Task 1D pt 1.py

---
+++ Task 1D pt 1.py
@@ -0,0 +1,21 @@
+from floodsystem.geo import stations_by_river
+from floodsystem.stationdata import build_station_list
+
+def run():
+    """Requirements for Task 1D"""
+     #Build list of stations 
+    stations = build_station_list()
+
+    #build a list of the list given at the end of 1D.1
+    rivwstat= list(rivers_with_station(stations))
+
+    #print number of rivers with at least one station
+    print (len(rivwstat))
+
+    #print alphabetical list of the first 10 rivers with a station
+    sortedriv= rivwstat.sort()
+    for i in range (:10):
+        print sortedriv(i)
+if __name__ == "__main__":
+    print("*** Task 1D Part 1: CUED Part IA Flood Warning System ***")
+    run()
