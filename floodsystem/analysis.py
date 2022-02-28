ea041bffcbefdef2965673903d2bdd4f39134a8e
Chloe <nfcc2@cam.ac.uk>
2022-02-28 17:44:41 +0000

    Added forecast func for 2G

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,6 +2,7 @@
 
 from matplotlib.dates import date2num
 import numpy as np
+import datetime
 
 def polyfit(dates, levels, p):
     """This function computes a least-squares fit of a polynomial of degree p to water level data"""
@@ -10,4 +11,13 @@ def polyfit(dates, levels, p):
     p_coeff = np.polyfit(x-x[0], levels, p)
 
     return np.poly1d(p_coeff), x[0]
+
+def forecast(poly, d0):
+    """This function predicts whether the water level at a station is rising or falling"""
+    today = date2num(datetime.date.today())
+
+    # use gradient of polynomial to find out if water level is rising or falling
+    # positive gradient = rising, negative gradient = falling
+    d = poly.deriv()
+    return d(today-d0)
 
