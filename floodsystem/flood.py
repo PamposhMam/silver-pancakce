c886c1cb5adc47354ea17a1bfa5774f8b44fc642
Chloe <nfcc2@cam.ac.uk>
2022-02-28 20:47:21 +0000

    added 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,9 @@
 """This module contains functions related to flooding and water levels
 
 """
+
+from floodsystem.station import MonitoringStation
+
 #it says to do this in the submodule "flood"... I can't find that
 def stations_level_over_threshold(stations, tol):
     """return a list of tuples with 1) a station (object) and 2) the station's relative water level. Note, this should only work if the level is> a given tolerance."""
@@ -17,3 +20,12 @@ def stations_level_over_threshold(stations, tol):
         statnames[statname].append(MonitoringStation)
     stattuple=list(zip(statnames,statlevels))
     stattuple.sort(reverse=True)
+
+def stations_highest_rel_level(stations, N):
+    """This function returns a list of N stations at which the relative water level is the highest"""
+
+    stations = [(station, MonitoringStation.relative_water_level(station)) for station in stations if station.typical_range_consistent()]
+    stations.sort(key = itemgetter(1), reverse = True)
+
+    return [s[0] for s in stations]
+
