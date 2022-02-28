08c27bbca34a5c7f07f4af0faf8e33574c2a8db7
Chloe <nfcc2@cam.ac.uk>
2022-02-28 21:16:26 +0000

    Added function to create a sample of stations

--- test_station.py
+++ test_station.py
@@ -6,6 +6,8 @@
 from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 from floodsystem.stationdata import build_station_list
 
+import random
+
 
 def test_create_monitoring_station():
 
@@ -58,3 +60,11 @@ def test_inconsistent_typical_range_stations():
         # test if stations on the list actually have inconsisdent data
         assert MonitoringStation.typical_range_consistent(inconsistentStations[0]) == False
 
+def create_sample_stations():
+    """Creates list of 10 stations objects for testing range and water levels"""
+    
+    stations = [MonitoringStation("test-s-id", "test-s-id", str(i+1), (0, 0), (random.random(), 1+random.random()), "river", "town") for i in range(10)]
+    for s in stations:
+        s.latest_level = random.random() + random.randrange(0, 1)
+
+    return stations
