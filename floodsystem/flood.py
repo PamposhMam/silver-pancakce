c6cba7862bb9e7d65d0b325333138f993786d187
PamposhMam <99150494+PamposhMam@users.noreply.github.com>
2022-02-28 14:23:35 +0000

    Update flood.py
    Yeah... well, if this doesn't work out, we can always add it under station.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,3 +1,19 @@
 """This module contains functions related to flooding and water levels
 
 """
+#it says to do this in the submodule "flood"... I can't find that
+def stations_level_over_threshold(stations, tol):
+    """return a list of tuples with 1) a station (object) and 2) the station's relative water level. Note, this should only work if the level is> a given tolerance."""
+    """Also sorted in descending order!"""
+    statnames= {}
+    statlevels= {}
+    for station in stations:
+        if relative_water_level>tol:
+            statlevel= MonitoringStation.relative_water_level 
+            statlevels[statlevel].append(MonitoringStation)
+    statlevel.sort(reverse=True)
+    for station in statlevel:
+        statname= MonitoringStation.name
+        statnames[statname].append(MonitoringStation)
+    stattuple=list(zip(statnames,statlevels))
+    stattuple.sort(reverse=True)
