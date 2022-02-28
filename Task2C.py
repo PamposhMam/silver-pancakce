4c21a7fb9f001ee339b995d54ec0b42a150f9eb3
Chloe <nfcc2@cam.ac.uk>
2022-02-28 20:47:55 +0000

    added 2C

---
+++ Task2C.py
@@ -0,0 +1,16 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.station import MonitoringStation
+
+def run():
+    """requirements for 2C"""
+
+    stations = build_station_list()
+    stations = stations_highest_rel_level(stations, 10)
+
+    for station in stations:
+        print(station.name, MonitoringStation.relative_water_level(station))
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
