5c4576e6e7971ece4e0cad8c4c50b44a1927c29c
Chloe <nfcc2@cam.ac.uk>
2022-03-01 11:49:28 +0000

    Completed 2G

--- Task2G.py
+++ Task2G.py
@@ -2,7 +2,7 @@
 import datetime
 
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import towns_by_rel_levels
+from floodsystem.flood import towns_by_rel_levels, floodWarning
 
 def run():
     "2G"
@@ -10,17 +10,22 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
 
-    # get list of (town, highest rel lev) tuples, in descending order
+    # get list of (town, highest rel lev, rel predicted lev change) tuples, in descending order, for towns above 0.8
     towns = towns_by_rel_levels(stations)
-    print(towns)
+
+    #print(sorted([town[2] for town in towns]))
 
     # assign risk rating to each town
     # there are anomolies in the relative levels (600, -16 etc. typical range 2.5 - -1.5)
+    print('\n***FLOOD WARNINGS***\n')
 
-    # factor in predicted change
+    for town in towns:
+        floodWarning(town)
+        #print(town[1], town[2])
 
     return
 
 if __name__ == "__main__":
     print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
+
