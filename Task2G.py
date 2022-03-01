b3cf5f94bf717d0b92bd4a6691c505b8588c322d
Chloe <nfcc2@cam.ac.uk>
2022-03-01 01:34:49 +0000

    did function that returns towns in order of risk

--- Task2G.py
+++ Task2G.py
@@ -1,21 +1,23 @@
 
 import datetime
 
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import towns_by_rel_levels
 
 def run():
     "2G"
 
     stations = build_station_list()
+    update_water_levels(stations)
 
     # get list of (town, highest rel lev) tuples, in descending order
     towns = towns_by_rel_levels(stations)
+    print(towns)
 
     # assign risk rating to each town
 
     # factor in predicted change
-    
+
     return
 
 if __name__ == "__main__":
