a0721d76fb1bd353eb40e09444225e06eeea75aa
PamposhMam <99150494+PamposhMam@users.noreply.github.com>
2022-03-01 00:32:01 +0000

    Update Task2B.py

--- Task2B.py
+++ Task2B.py
@@ -6,4 +6,4 @@ from floodsystem.stationdata import update_water_levels
 
 stations= build_station_list()
 water_level= update_water_levels(stations)
-print(stations_level_over_threshold(stations, 0.8)
+print(stations_level_over_threshold(stations, 0.8))
