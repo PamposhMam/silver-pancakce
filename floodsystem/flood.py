005812abc8dd5477bc17abf2f09faf8371722d7b
Chloe <nfcc2@cam.ac.uk>
2022-03-01 11:29:38 +0000

    Added warning function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -71,9 +71,56 @@ def towns_by_rel_levels(stations):
     #                 sortedTowns.append((town, sortedLevels[i]))
     
     for town in towns:
+        if town == None:
+            continue
         sortedTowns.append([town, towns[town][0], towns[town][1]])
 
     # return list of towns in descending order of risk [[town, rev level, predicted change]]
     return sortedTowns
 
+def floodWarning(town):
+    """2G: prints an evaluation flood risk based on relative levels and predicted water level change. """
 
+    level = town[1]
+    change = round(town[2], 2)
+    predictedLevel = level + change
+
+    print("\nTown: {}".format(town[0]))
+
+    if level < 1:
+        risk = "low"
+    elif level < 1.4:
+        risk = "moderate"
+    elif level < 1.8:
+        risk = "high"
+    else:
+        risk = "severe"
+
+    if change < -2:
+        phrase = "fall significantly by {} metres :D".format(str(change)[1:])
+    elif change < -1:
+        phrase = "fall by {} metres :)".format(str(change)[1:])
+    elif change < -0.25:
+        phrase = "fall slightly by {} metres".format(str(change)[1:])
+    elif change  < 0.25:
+        phrase = "stay the same"
+    elif change < 1:
+        phrase = "rise slightly by {} metres".format(str(change))
+    elif change < 2:
+        phrase = "rise by {} metres :0".format(str(change))
+    else:
+        phrase = "rise significantly by {} metres :(".format(str(change))
+
+    if predictedLevel > 1.4 and level < 1.4:
+        warning = "Moderate"
+    elif predictedLevel > 1.8 and level < 1.4:
+        warning = "High"
+    elif predictedLevel >= 1.8 and level < 1.4:
+        warning = "Severe"
+    else: warning = None
+
+    print("Flood risk: {}".format(risk))
+    print("River water levels are predicted to " + phrase)
+
+    if warning:
+        print("Be careful: {} risk of flooding within the next day".format(warning))
